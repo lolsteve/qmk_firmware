c939d5bd616ed3d979084c5539dd4688ff825507
Steven Wallace <steven@thewallaces.ca>
2018-09-10 21:49:42 -0700

    Refractored Steve GH60

--- keyboards/gh60/keymaps/steve/keymap.c
+++ keyboards/gh60/keymaps/steve/keymap.c
@@ -1,6 +1,4 @@
-#include "gh60.h"
-#include "action_layer.h"
-#include "rgblight.h"
+#include QMK_KEYBOARD_H
 
 #define _DEFAULT 0
 #define _FN 1
@@ -15,7 +13,7 @@ enum planck_keycodes {
 #define ______ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Qwerty gui/alt/space/alt/gui
+/*
  * ,-----------------------------------------------------------------------------------------.
  * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  \  |  `  |
  * |-----------------------------------------------------------------------------------------+
@@ -25,15 +23,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------------------------------------------------------+
  * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RShift   | Down |
  * |-----------------------------------------------------------------------------------------+
- * |  FN |  LAlt |  LGUI |              Space                     | RGUI | RAlt | RCTRL | FN |
+ * |  FN |  LGUI |  LALT |              Space                     | RALT | RGUI | RCTRL | FN |
  * `-----------------------------------------------------------------------------------------'
  */
-  [_DEFAULT] = KEYMAP_HHKB( /* Basic QWERTY */
+  [_DEFAULT] = LAYOUT_60_ansi_split_bs_rshift(
       KC_ESC,  KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_6, KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV, \
       KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,  \
       KC_LCTL, KC_A,    KC_S,    KC_D, KC_F, KC_G,   KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  \
       KC_LSFT, KC_Z,    KC_X,    KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_DOWN, \
-      MO(_FN), KC_LALT, KC_LGUI,                  KC_SPC,                 KC_RGUI, KC_RALT, KC_RCTL, MO(_FN) \
+      MO(_FN), KC_LGUI, KC_LALT,                  KC_SPC,                 KC_RALT, KC_RGUI, KC_RCTL, MO(_FN) \
       ),
 
 /* FN Layer
@@ -49,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |       |       |                                             |       |       |     |     |
  * `-----------------------------------------------------------------------------------------'
  */
-  [_FN] = KEYMAP_HHKB( /* Layer 1 */
+  [_FN] = LAYOUT_60_ansi_split_bs_rshift( /* Layer 1 */
       ______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12, KC_INS,  KC_DEL, \
       KC_CAPS, ______,  ______,  ______,  ______, ______, ______,  ______,  ______, ______,   ______,  ______, ______, KC_DEL,  \
       ______,  KC_VOLD, KC_VOLU, KC_MUTE, ______, ______, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, ______,  ______, ______,   \
